aa6ff22202197095d5f8698cfc42749b4a754461
dragosbus <dragosbusuioc94@gmail.com>
2018-05-09 14:59:41 +0200

    init github finder

--- src/App.js
+++ src/App.js
@@ -1,52 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      counter: 0
-    }
-    this.clickHandler = this.clickHandler.bind(this);
-  }
 
-  clickHandler(amount) {
-    this.setState(prevState => {
-      return {
-        counter: prevState.counter + amount
-      }
-    });
-    console.log(this.state.counter)
-  }
-
-  render() {
-    return (
-      <div className="app">
-        <Button amount={1} callParent={this.clickHandler} />
-        <Button amount={2} callParent={this.clickHandler} />
-        <Result value={this.state.counter}/>
-      </div>
-    );
-  }
-}
-
-class Button extends Component {
-  constructor(props) {
-    super(props);
-    this.clickHandler = this.clickHandler.bind(this);
-  }
-
-  clickHandler() {
-    this.props.callParent(this.props.amount)
-  }
-
-  render() {
-    return (
-      <button onClick={this.clickHandler}>{this.props.amount}</button>
-    );
-  }
-}
-
-const Result = props => <h3>{props.value}</h3>
 
 export default App;
