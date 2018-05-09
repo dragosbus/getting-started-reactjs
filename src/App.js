1d99dc0fab632ea65d8f4d1d29eb8e2da7c460e9
dragosbus <dragosbusuioc94@gmail.com>
2018-05-09 14:55:05 +0200

    create a button reusable button component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,52 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      counter: 0
+    }
+    this.clickHandler = this.clickHandler.bind(this);
+  }
+
+  clickHandler(amount) {
+    this.setState(prevState => {
+      return {
+        counter: prevState.counter + amount
+      }
+    });
+    console.log(this.state.counter)
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="app">
+        <Button amount={1} callParent={this.clickHandler} />
+        <Button amount={2} callParent={this.clickHandler} />
+        <Result value={this.state.counter}/>
       </div>
     );
   }
 }
+
+class Button extends Component {
+  constructor(props) {
+    super(props);
+    this.clickHandler = this.clickHandler.bind(this);
+  }
+
+  clickHandler() {
+    this.props.callParent(this.props.amount)
+  }
+
+  render() {
+    return (
+      <button onClick={this.clickHandler}>{this.props.amount}</button>
+    );
+  }
+}
+
+const Result = props => <h3>{props.value}</h3>
 
 export default App;
